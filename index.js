a463729ce6fc223128d43ffc7a7b0b8bedb8f1c0
zaephyr <uros.mahne@gmail.com>
2020-06-15 13:13:32 +0200

    finished Library app without data storage

---
+++ index.js
@@ -0,0 +1,188 @@
+const myLibrary = [];
+const tbody = document.querySelector('#books');
+
+class Book {
+    constructor(title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+}
+
+myLibrary[0] = new Book('The Black Swan', 'Nassim Nicholas Taleb', 400, true);
+myLibrary[1] = new Book('Crime and Punishement', 'Fyodor Dostoevsky', 576, true);
+myLibrary[2] = new Book('The Hobit', 'J.R.R. Tolkien', 295, false);
+
+function addBookButton() {
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+
+    if (title.value.length >= 2 && author.value.length >= 2) {
+        const newBook = new Book(title.value, author.value, pages.value, false);
+        myLibrary.push(newBook);
+    } else {
+        alert('You need to fill title and author');
+    }
+    updateTable();
+}
+
+function removeBook(ele) {
+    const selectRow = ele.parentNode.parentNode.firstChild.textContent;
+    const index = myLibrary.findIndex((obj) => obj.title == selectRow);
+    myLibrary.splice(index, 1);
+    updateTable();
+}
+
+function changeStatus(ele) {
+    const selectRow = ele.parentNode.parentNode.firstChild.textContent;
+    const index = myLibrary.findIndex((obj) => obj.title == selectRow);
+    if (myLibrary[index].status) {
+        myLibrary[index].status = false;
+    } else {
+        myLibrary[index].status = true;
+    }
+    updateTable();
+}
+
+function nameShort() {
+    const nameCell = document.querySelectorAll('td:nth-child(2)');
+    const nameArr = [];
+    for (let i = 0; i < nameCell.length; i++) {
+        nameArr.push(nameCell[i].textContent);
+    }
+    // window.addEventListener('resize', () => {
+    //     console.log('resize');
+    const nameCellWidth = document.querySelectorAll('td:nth-child(2)')[0].clientWidth;
+    if (nameCellWidth < 175) {
+        for (let i = 0; i < nameArr.length; i++) {
+            if (nameArr[i].length > 14) {
+                const singleNameArr = nameArr[i].split(' ');
+                if (singleNameArr.length == 2) {
+                    nameCell[i].textContent = singleNameArr[0].toString().charAt(0) + '. ' + singleNameArr[1];
+                } else if (singleNameArr.length == 3) {
+                    nameCell[i].textContent =
+                        singleNameArr[0].toString().charAt(0) +
+                        '. ' +
+                        singleNameArr[1].toString().charAt(0) +
+                        '. ' +
+                        singleNameArr[2];
+                }
+            }
+        }
+    }
+}
+//);
+// }
+
+function updateTable(arr) {
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const row = document.createElement('tr');
+
+        const title = document.createElement('td');
+        title.innerText = myLibrary[i].title;
+
+        const author = document.createElement('td');
+        author.innerText = myLibrary[i].author;
+
+        const pages = document.createElement('td');
+        pages.innerText = myLibrary[i].pages;
+
+        const status = document.createElement('td');
+        const statusButton = document.createElement('button');
+        statusButton.setAttribute('onclick', 'changeStatus(this)');
+        statusButton.className = 'checkbox';
+        statusButton.style.height = '100%';
+        statusButton.style.backgroundColor = 'inherit';
+        statusButton.style.fontSize = '1rem';
+        statusButton.style.border = 0;
+        statusButton.style.padding = 0;
+
+        if (myLibrary[i].status) {
+            const check = document.createElement('ion-icon');
+            check.name = 'shield-checkmark-outline';
+            check.style.color = '#81ecec';
+            statusButton.appendChild(check);
+        } else if (!myLibrary[i].status) {
+            const notRead = document.createElement('ion-icon');
+            notRead.name = 'close-circle-outline';
+            notRead.style.color = '#fd79a8';
+            statusButton.appendChild(notRead);
+        }
+
+        const delIconPlace = document.createElement('td');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.class = 'delete-button';
+        deleteButton.setAttribute('onclick', 'removeBook(this)');
+        deleteButton.style.backgroundColor = 'inherit';
+        deleteButton.style.fontSize = '1rem';
+        deleteButton.style.border = 0;
+        deleteButton.style.padding = 0;
+
+        const delIcon = document.createElement('ion-icon');
+        delIcon.name = 'trash-outline';
+
+        tbody.appendChild(row);
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(pages);
+        status.appendChild(statusButton);
+        row.appendChild(status);
+        delIconPlace.appendChild(deleteButton);
+        deleteButton.appendChild(delIcon);
+        row.appendChild(delIconPlace);
+        nameShort();
+    }
+}
+
+const filterButtons = document.querySelectorAll('.filter');
+filterButtons.forEach((filterButton) => {
+    filterButton.addEventListener('click', () => {
+        filterButtons.forEach((filterButton) => {
+            filterButton.classList.remove('active');
+        });
+        filterButton.classList.toggle('active');
+        let filter = filterButton.getAttribute('data-filter');
+        let checkbox = document.querySelectorAll('.checkbox');
+
+        if (filter == 'all-books') {
+            const filterStatus = document.querySelector('.filter-status');
+            filterStatus.textContent = 'All Books';
+            checkbox.forEach((checkbox) => {
+                let row = checkbox.parentNode.parentNode;
+                row.style.display = 'table-row';
+            });
+            return;
+        }
+        checkbox.forEach((checkbox) => {
+            let row = checkbox.parentNode.parentNode;
+            const selectRow = checkbox.parentNode.parentNode.firstChild.textContent;
+            const index = myLibrary.findIndex((obj) => obj.title == selectRow);
+            const filterStatus = document.querySelector('.filter-status');
+            if (filter == 'read') {
+                filterStatus.textContent = 'Read Books';
+                if (myLibrary[index].status) {
+                    row.style.display = 'table-row';
+                } else {
+                    row.style.display = 'none';
+                }
+                return;
+            } else if (filter == 'not-read') {
+                filterStatus.textContent = 'Not Read Books';
+                if (myLibrary[index].status) {
+                    row.style.display = 'none';
+                } else {
+                    row.style.display = 'table-row';
+                }
+                return;
+            }
+        });
+    });
+});
+
+updateTable();
+nameShort();
